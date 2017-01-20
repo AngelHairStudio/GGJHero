7010012e299b413b43ca4fa56fb4f30a41594eed
Lindstedt Laptop <marcus.t.lindstedt@gmail.com>
2017-01-20 22:52:04 +0100

    Basic movement for the charcter

--- AngelHairBro/Assets/Scripts/PlayerController.cs
+++ AngelHairBro/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    private float m_maxSpeed = 10f;
 
-	// Use this for initialization
-	void Start () {
+    private bool m_facingRight = true;
+    private bool m_facingDown = true;
+
+    private Vector2 m_move;
+
+	void Start ()
+    {
 		
 	}
+
+    /// <summary>
+    /// Physics
+    /// </summary>
+    void FixedUpdate()
+    {
+        m_move.x = Input.GetAxis("Horizontal");
+        m_move.y = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(m_move.x * m_maxSpeed, m_move.y * m_maxSpeed);
+
+        if (m_move.x > 0 && !m_facingRight)
+        {
+            FlipChar();
+        }
+        else if(m_move.x < 0  && m_facingRight)
+        {
+            FlipChar();
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    public float GetSpeed()
+    {
+        return m_maxSpeed;
+    }
+
+    void FlipChar()
+    {
+        m_facingRight = !m_facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
