3f80887618ac9004b97d3517179f7d8c87162492
Gordox <antonsvensson94@hotmail.se>
2017-01-21 02:43:19 +0100

    Started on the basic of the enemie class and its base class

---
+++ AngelHairBro/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Entity {
+
+    public enum State { AttackingPlayer, AttackingTower };
+    State currentState;
+
+    Transform playerTarget;
+
+
+    float damage = 1;
+    float attackingRadius;
+    float attackDelay;
+
+
+
+    bool hasPlayerAsTarget;
+
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
+        currentState = State.AttackingTower;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetCharacteristics(int damage, float enemyHealth, float moveSpeed)
+    {
+        this.damage = damage;
+        startingHealth = enemyHealth;
+
+    }
+}
