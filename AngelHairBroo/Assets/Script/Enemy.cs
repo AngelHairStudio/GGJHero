51bb94b0706383bce8b1b9528478af7df28c9bdc
Gordox <antonsvensson94@hotmail.se>
2017-01-21 16:54:02 +0100

    Some thing

--- AngelHairBroo/Assets/Script/Enemy.cs
+++ AngelHairBroo/Assets/Script/Enemy.cs
@@ -11,10 +11,16 @@ public class Enemy : Entity
     Transform playerTarget;
     Transform castleTarget;
     Entity castleEntity;
+    Entity playerEntity;
     UnityEngine.AI.NavMeshAgent pathfinder;
 
-    float damage = 1;
-    float attackingRadius;
+    [SerializeField]
+    private float damage = 1;
+    [SerializeField]
+    private float attackSpped = 1;
+    [SerializeField]
+    private float attackingRadius;
+
     float attackDelay;
 
 
@@ -30,9 +36,14 @@ public class Enemy : Entity
             hasCastleAsTarget = true;
             castleTarget = GameObject.FindGameObjectWithTag("Castle").transform;
             castleEntity = castleTarget.GetComponent<Entity>();
-
-         
         }
+
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
+            playerEntity = castleTarget.GetComponent<Entity>();
+        }
+
     }
 
     // Use this for initialization
@@ -50,7 +61,7 @@ public class Enemy : Entity
     // Update is called once per frame
     void Update()
     {
-
+        //if((playerTarget.position - transform.position))
     }
 
     public void SetCharacteristics(int damage, float enemyHealth, float moveSpeed)
@@ -81,6 +92,47 @@ public class Enemy : Entity
         }
     }
 
+    IEnumerator Attack()
+    {
+        if (hasPlayerAsTarget)
+            currentState = State.AttackingPlayer;
+        else if (hasCastleAsTarget)
+            currentState = State.AttackingTower;
+
+        pathfinder.enabled = false;
+
+        Vector3 dirToTarget;
+
+        switch (currentState)
+        {
+            case State.AttackingPlayer:
+                dirToTarget = (playerTarget.position - transform.position).normalized;
+                break;
+            case State.AttackingTower:
+                dirToTarget = (castleTarget.position - transform.position).normalized;
+                break;
+            default:
+                break;
+        }
+
+        float percent = 0;
+        bool haveAttacked = false;
+        while (percent <= 1)
+        {
+            if(percent >= .5f && !haveAttacked)
+            {
+                haveAttacked = true;
+                //Create projectile here
+                Debug.Log("Enemy projectile created");
+            }
+
+            percent += Time.deltaTime * attackSpped;
+            yield return null;
+
+        }
+
+        pathfinder.enabled = true;
+    }
 
     void OnTargetDeath()
     {
