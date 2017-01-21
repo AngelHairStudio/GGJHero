7d5457f9c3660cdc45dbc6476fda0b046ff30a3a
Lindstedt Laptop <marcus.t.lindstedt@gmail.com>
2017-01-21 12:34:10 +0100

    Added movement and rotation to player

--- AngelHairBroo/Assets/Script/PlayerController.cs
+++ AngelHairBroo/Assets/Script/PlayerController.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    private float m_speed = 10.0f;
+    private int m_floorMask;
+    private Vector3 m_movement;
+    private Rigidbody m_playerRB;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    void Awake()
+    {
+        m_floorMask = LayerMask.GetMask("Floor");
+        m_playerRB = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+
+    }
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        MovePlayer(moveHorizontal, moveVertical);
+
+    }
+
+    void MovePlayer(float horiz, float vertic)
+    {
+        m_movement.Set(horiz, 0.0f, vertic);
+        m_movement = m_movement.normalized * m_speed * Time.deltaTime;
+        m_playerRB.MovePosition(transform.position + m_movement);
+        transform.rotation = Quaternion.LookRotation(m_movement.normalized);
+    }
 }
